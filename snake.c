8a2508bf39f8a75de20e7ba5d27e6d093e84bcec
Zane Globus-O'Harra <zanegloh@gmail.com>
2024-01-02 12:22:19 -0800

    Initial commit

---
+++ snake.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define UNUSED __attribute__((unused))
+
+int main(UNUSED int argc, UNUSED char *argv[])
+{
+    return 0;
+}
